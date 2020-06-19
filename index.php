ace5f6b6d93029ac5b8dfc56c6f99e65c2601125
IVAN BLANCO <ivanblanco@icel.edu.mx>
2020-06-18 20:41:44 -0500

    Repositorio Incio Sistema CSA Ser

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenidos</title>
+    <link rel="stylesheet" href="css/materialize.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script src="js/materialize.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+</head>
+<body style="background-color: #b2ebf2;" >
+    <div class="container">
+        <div class="row" style="margin-top: 5%;">
+        <div class="col s4 offset-s2 grey lighten-3 center-align">
+        <h3 style="color: #01579b;">CONSULTORIO DE MEDICINA ALTERNATIVA SER</h3>
+        <div class="divider"></div>
+        <h5>Iniciar Sesión</h5>
+        <div class="row">
+        <form class="col s12">
+        <div class="row">
+            <div class="input-field col s12">
+            <i class="material-icons prefix">account_circle</i>
+            <input id="icon_prefix" type="text" name="user" class="validate" required>
+            <label for="icon_prefix">Usuario</label>
+            </div>
+            <div class="input-field col s12">
+            <i class="material-icons prefix">vpn_key</i>
+            <input id="password" type="password" class="validate" required>
+            <label for="password">Password</label>
+            </div>
+            <div class="col s12">
+                <button class="btn" style="background-color: #01579b;" type="submit" name="action">Acceder
+                    <i class="material-icons right">send</i>
+                </button>
+            </div>
+        </div>
+        </form>
+        </div>
+        <blockquote>
+            ¿Olvidaste tu contraseña? Ponte en contacto con el Administrador del Sistema
+        </blockquote>
+        <br>
+    </div>
+           
+        
+        <div class="col s4">
+        <img src="" alt="">
+        </div>
+        </div>
+    </div>
+    
+</body>
+</html>
